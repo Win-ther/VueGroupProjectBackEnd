620fd574278a838b7b269871fb445089eb64e56c
Win-ther <emil.winther@hotmail.com>
2024-10-14 12:57:47 +0200

    Add entities and Flyway-migration, docker-compose and dockerignore.
    Co-authored-by: Katherine Altmann <katherine.altmann@iths.se>
    Co-authored-by: Trung Ma <trung.ma@iths.se>

---
+++ src/main/java/se/iths/vuegroupprojectbackend/entities/Car.java
@@ -0,0 +1,43 @@
+package se.iths.vuegroupprojectbackend.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.proxy.HibernateProxy;
+
+import java.util.Objects;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "car")
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    private String image;
+    private String model;
+    private int popularity;
+    private int seats;
+    private String transmission;
+    private String fuel;
+    private int price;
+    private String about;
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Car car = (Car) o;
+        return getId() != null && Objects.equals(getId(), car.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
+}
---
+++ src/main/resources/db/migration/V1__.sql
@@ -0,0 +1,28 @@
+CREATE TABLE car
+(
+    id           BIGINT AUTO_INCREMENT NOT NULL,
+    image        VARCHAR(255)          NULL,
+    model        VARCHAR(255)          NULL,
+    popularity   INT                   NOT NULL,
+    seats        INT                   NOT NULL,
+    transmission VARCHAR(255)          NULL,
+    fuel         VARCHAR(255)          NULL,
+    price        INT                   NOT NULL,
+    about        VARCHAR(255)          NULL,
+    CONSTRAINT pk_car PRIMARY KEY (id)
+);
+
+CREATE TABLE reservation
+(
+    id             BIGINT AUTO_INCREMENT NOT NULL,
+    date_from      date                  NULL,
+    date_to        date                  NULL,
+    car_id         BIGINT                NULL,
+    customer_email VARCHAR(255)          NULL,
+    customer_name  VARCHAR(255)          NULL,
+    customer_phone VARCHAR(255)          NULL,
+    CONSTRAINT pk_reservation PRIMARY KEY (id)
+);
+
+ALTER TABLE reservation
+    ADD CONSTRAINT FK_RESERVATION_ON_CAR FOREIGN KEY (car_id) REFERENCES car (id);
