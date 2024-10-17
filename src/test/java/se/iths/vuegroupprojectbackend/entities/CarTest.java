91ba3dec3a225b04c16d73fe721e56d1d1398b42
Win-ther <emil.winther@hotmail.com>
2024-10-17 10:47:46 +0200

    Add Tests for coverage on entities
    Co-authored-by: Katherine Altmann <katherine.altmann@iths.se>
    Co-authored-by: Trung Ma <trung.ma@iths.se>

--- src/test/java/se/iths/vuegroupprojectbackend/entities/CarTest.java
+++ src/test/java/se/iths/vuegroupprojectbackend/entities/CarTest.java
@@ -1,24 +1,48 @@
 package se.iths.vuegroupprojectbackend.entities;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CarTest {
+    private Car car;
+    private Car car2;
+
+
+    @BeforeEach
+    void setUp() {
+        car = new Car(1L, "imagelink", "model", 5, 4, "Automatic", "Petrol", 200, "Car description");
+        car2 = new Car(2L, "imageLink", "model", 5, 4, "Automatic", "Petrol", 200, "Car description");
+    }
 
     @Test
     void increasePopularity() {
+        int initialPopularity = car.getPopularity();
+        car.increasePopularity();
+        assertEquals(initialPopularity + 1, car.getPopularity(), "Popularity should increase by 1");
     }
 
     @Test
     void decreasePopularity() {
+        int initialPopularity = car.getPopularity();
+        car.decreasePopularity();
+        assertEquals(initialPopularity - 1, car.getPopularity(), "Popularity should decrease by 1");
     }
 
     @Test
     void testEquals() {
+        Car car3 = car;
+        assertEquals(car, car3, "Cars with the same ID should be the same");
+    }
+
+    @Test
+    void testNotEquals() {
+        assertNotEquals(car, car2, "Cars with the same ID should not be the same");
     }
 
     @Test
     void testHashCode() {
+        assertEquals(car.hashCode(), car2.hashCode(), "Cars with the same ID should have the same hash code");
     }
 }
--- src/test/java/se/iths/vuegroupprojectbackend/entities/ReservationTest.java
+++ src/test/java/se/iths/vuegroupprojectbackend/entities/ReservationTest.java
@@ -1,20 +1,63 @@
 package se.iths.vuegroupprojectbackend.entities;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.sql.Date;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ReservationTest {
 
+    private Reservation reservation;
+    private Reservation reservation2;
+
+    @BeforeEach
+    void setUp() {
+        Car car = new Car(2L, "image", "model", 420, 4, "Automatic", "Electric", 300, "aboot");
+
+        reservation = new Reservation();
+        reservation.setId(1L);
+        reservation.setDateFrom(Date.valueOf("2024-03-12"));
+        reservation.setDateTo(Date.valueOf("2025-03-12"));
+        reservation.setCar(car);
+        reservation.setCustomerEmail("Email@email.email");
+        reservation.setCustomerName("Jeff Burnham");
+        reservation.setCustomerPhone("0808050505");
+
+        reservation2 = new Reservation();
+        reservation2.setId(2L);
+        reservation2.setDateFrom(Date.valueOf("2024-03-12"));
+        reservation2.setDateTo(Date.valueOf("2025-03-12"));
+        reservation2.setCar(car);
+        reservation2.setCustomerEmail("Email@email.email");
+        reservation2.setCustomerName("Jeff Burnham");
+        reservation2.setCustomerPhone("0808050505");
+
+
+    }
+
     @Test
     void generateReservationNumber() {
+        reservation.setReservationNumber(reservation.generateReservationNumber());
+
+        assertThat(reservation.getReservationNumber()).isEqualTo("J505242008");
     }
 
     @Test
     void testEquals() {
+        Reservation reservation3 = reservation;
+        assertEquals(reservation, reservation3, "Reservations with the same ID should be the same");
+    }
+
+    @Test
+    void testNotEquals() {
+        assertNotEquals(reservation, reservation2, "Reservations with the same ID should not be the same");
     }
 
     @Test
     void testHashCode() {
+        assertEquals(reservation.hashCode(), reservation.hashCode(), "Reservations with the same ID should have the same hash code");
     }
 }
